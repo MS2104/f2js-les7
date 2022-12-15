3ee252d037f7810b29eeab54a4a75bb15d326b00
MS2104 <31002@ma-web.nl>
2022-12-15 10:08:51 +0100

    ok

---
+++ classstappen.js
@@ -0,0 +1,14 @@
+class discordgebruiker {
+        constructor() {
+            this.gebruikersnaam
+            this.tag
+            this.userid
+            // this.profielfoto
+        }
+
+        gebruikersacties (gebruikersnaam, tag, userid) {
+            this.gebruikersnaam = 'Jan';
+            this.tag = "0001";
+            this.userid = "5439153010";
+        }
+    }
