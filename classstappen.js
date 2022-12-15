e096bbd761f6566da91e88b1b2f5fc48de7ca535
MS2104 <31002@ma-web.nl>
2022-12-15 10:17:27 +0100

    ok versie ik ben de tel kwijt xd

--- classstappen.js
+++ classstappen.js
@@ -3,6 +3,8 @@ class discordgebruiker {
             this.gebruikersnaam = 'Jan';
             this.tag = '0001';
             this.userid = '5439153010';
+            this.heeftNitro = True;
+            this.NitroGebruikerSinds = '1/1/2022';
             // this.profielfoto = (link naar profiel foto)
         }
 
@@ -10,5 +12,7 @@ class discordgebruiker {
             this.gebruikersnaam = gebruikersnaam;
             this.tag = tag;
             this.userid = userid;
+            this.heeftNitro = this.heeftNitro;
+            this.NitroGebruikerSinds = this.NitroGebruikerSinds;
         }
     }
